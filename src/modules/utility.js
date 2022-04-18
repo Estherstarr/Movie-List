5f3b954b51d074716553827fa64c10f32c6fc04d
onomeh <onwuanibeonome@gmail.com>
2022-04-18 19:51:57 +0000

    Fix linter error

--- src/modules/utility.js
+++ src/modules/utility.js
@@ -2,9 +2,6 @@ const showApi = 'https://api.tvmaze.com/shows';
 
 const fetchShows = async () => {
   const res = await fetch(showApi);
-  const allShows = await res.json();
-  return allShows;
+  return res.json();
 };
-export {
-  fetchShows, showApi,
-};
+export default { fetchShows };
